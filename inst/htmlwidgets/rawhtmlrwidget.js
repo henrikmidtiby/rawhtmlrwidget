60266a3fd0c7a9306c28466de881485f260baa54
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2017-01-31 12:29:53 +0100

    Added code for the widget.

---
+++ inst/htmlwidgets/rawhtmlrwidget.js
@@ -0,0 +1,31 @@
+HTMLWidgets.widget({
+
+  name: 'mywidget',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+      renderValue: function(x) {
+
+        // TODO: code to render the widget, e.g.
+        //el.innerText = x.message;
+        el.innerHTML = x.message;
+        // el.html = x.message;
+        //$(el).html('<h1>Hej</h1>');
+
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
